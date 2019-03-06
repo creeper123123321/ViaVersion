5cbebd8db61efbe06291e2f8183fb13da8369a18
creeper123123321 <creeper123123321@gmail.com>
2019-03-06 07:55:59 -0300

    Chunk as string

--- common/src/main/java/us/myles/ViaVersion/protocols/protocol1_13to1_12_2/storage/BlockConnectionStorage.java
+++ common/src/main/java/us/myles/ViaVersion/protocols/protocol1_13to1_12_2/storage/BlockConnectionStorage.java
@@ -1,13 +1,11 @@
 package us.myles.ViaVersion.protocols.protocol1_13to1_12_2.storage;
 
+import lombok.Synchronized;
 import us.myles.ViaVersion.api.Pair;
 import us.myles.ViaVersion.api.Via;
 import us.myles.ViaVersion.api.data.StoredObject;
 import us.myles.ViaVersion.api.data.UserConnection;
 import us.myles.ViaVersion.api.minecraft.Position;
-import us.myles.ViaVersion.api.minecraft.chunks.NibbleArray;
-import us.myles.ViaVersion.protocols.protocol1_13to1_12_2.data.MappingData;
-import us.myles.ViaVersion.protocols.protocol1_13to1_12_2.packets.WorldPackets;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
@@ -15,10 +13,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class BlockConnectionStorage extends StoredObject {
-    private Map<Long, Pair<byte[], NibbleArray>> blockStorage = createLongObjectMap();
+    private Map<Long, Pair<String, char[]>> blockStorage = createLongObjectMap();
 
     private static Constructor<?> fastUtilLongObjectHashMap;
-    private static HashMap<Short, Short> reverseBlockMappings;
 
     static {
         try {
@@ -26,83 +23,107 @@ public class BlockConnectionStorage extends StoredObject {
             Via.getPlatform().getLogger().info("Using FastUtil Long2ObjectOpenHashMap for block connections");
         } catch (ClassNotFoundException | NoSuchMethodException ignored) {
         }
-        reverseBlockMappings = new HashMap<>();
-        for (int i = 0; i < 4096; i++) {
-            int newBlock = MappingData.blockMappings.getNewBlock(i);
-            if (newBlock != -1) reverseBlockMappings.put((short) newBlock, (short) i);
-        }
+        Via.getPlatform().runRepeatingSync(new Runnable() {
+            @Override
+            public void run() {
+                Via.getPlatform().runAsync(new Runnable() {
+                    @Override
+                    public void run() {
+                        for (UserConnection info : Via.getManager().getPortedPlayers().values()) {
+                            BlockConnectionStorage storage = info.get(BlockConnectionStorage.class);
+                            if (storage != null) {
+                                if (info.getChannel().isOpen()) {
+                                    storage.transformToString();
+                                }
+                            }
+                        }
+                    }
+                });
+            }
+        }, 20L);
     }
 
     public BlockConnectionStorage(UserConnection user) {
         super(user);
     }
 
+    @Synchronized
     public void store(Position position, int blockState) {
-        Short mapping = reverseBlockMappings.get((short) blockState);
-        if (mapping == null) return;
-        blockState = mapping;
         long pair = getChunkSectionIndex(position);
-        Pair<byte[], NibbleArray> map = getChunkSection(pair, (blockState & 0xF) != 0);
+        Pair<String, char[]> map = getChunkSection(pair);
         int blockIndex = encodeBlockPos(position);
-        map.getKey()[blockIndex] = (byte) (blockState >> 4);
-        NibbleArray nibbleArray = map.getValue();
-        if (nibbleArray != null) nibbleArray.set(blockIndex, blockState);
+        char[] chars;
+        if (map.getKey() != null) {
+            chars = map.getKey().toCharArray();
+            map.setValue(chars);
+            map.setKey(null);
+        } else {
+            chars = map.getValue();
+        }
+        chars[blockIndex] = (char) blockState;
     }
 
+    @Synchronized
     public int get(Position position) {
         long pair = getChunkSectionIndex(position);
-        Pair<byte[], NibbleArray> map = blockStorage.get(pair);
+        Pair<String, char[]> map = blockStorage.get(pair);
         if (map == null) return 0;
         short blockPosition = encodeBlockPos(position);
-        NibbleArray nibbleArray = map.getValue();
-        return WorldPackets.toNewId(
-                ((map.getKey()[blockPosition] & 0xFF) << 4)
-                | (nibbleArray == null ? 0 : nibbleArray.get(blockPosition))
-        );
+        if (map.getKey() != null) {
+            return map.getKey().charAt(blockPosition);
+        } else {
+            return map.getValue()[blockPosition];
+        }
     }
 
+    @Synchronized
     public void remove(Position position) {
         long pair = getChunkSectionIndex(position);
-        Pair<byte[], NibbleArray> map = blockStorage.get(pair);
+        Pair<String, char[]> map = blockStorage.get(pair);
         if (map == null) return;
         int blockIndex = encodeBlockPos(position);
-        NibbleArray nibbleArray = map.getValue();
-        if (nibbleArray != null) {
-            nibbleArray.set(blockIndex, 0);
-            boolean allZero = true;
-            for (int i = 0; i < 4096; i++) {
-                if (nibbleArray.get(i) != 0) {
-                    allZero = false;
-                    break;
-                }
-            }
-            if (allZero) map.setValue(null);
+        char[] chars;
+        if (map.getKey() != null) {
+            chars = map.getKey().toCharArray();
+            map.setValue(chars);
+            map.setKey(null);
+        } else {
+            chars = map.getValue();
         }
-        map.getKey()[blockIndex] = 0;
-        for (short entry : map.getKey()) {
-            if (entry != 0) return;
+        chars[blockIndex] = (char) 0;
+        for (char block : chars) {
+            if (block != 0) return;
         }
         blockStorage.remove(pair);
     }
 
+    @Synchronized
     public void clear() {
         blockStorage.clear();
     }
 
+    @Synchronized
     public void unloadChunk(int x, int z) {
         for (int y = 0; y < 256; y += 16) {
             blockStorage.remove(getChunkSectionIndex(x << 4, y, z << 4));
         }
     }
 
-    private Pair<byte[], NibbleArray> getChunkSection(long index, boolean requireNibbleArray) {
-        Pair<byte[], NibbleArray> map = blockStorage.get(index);
-        if (map == null) {
-            map = new Pair<>(new byte[4096], null);
-            blockStorage.put(index, map);
+    @Synchronized
+    public void transformToString() {
+        for (Pair<String, char[]> pair : blockStorage.values()) {
+            if (pair.getValue() != null) {
+                pair.setKey(new String(pair.getValue()));
+                pair.setValue(null);
+            }
         }
-        if (map.getValue() == null && requireNibbleArray) {
-            map.setValue(new NibbleArray(4096));
+    }
+
+    private Pair<String, char[]> getChunkSection(long index) {
+        Pair<String, char[]> map = blockStorage.get(index);
+        if (map == null) {
+            map = new Pair<>(null, new char[4096]);
+            blockStorage.put(index, map);
         }
         return map;
     }
