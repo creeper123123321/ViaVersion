5a5ee062411add2ccadb4096b02b88d59cc02509
creeper123123321 <7974274+creeper123123321@users.noreply.github.com>
2019-11-02 17:26:24 -0300

    revert cancelexception changes

--- common/src/main/java/us/myles/ViaVersion/exception/CancelException.java
+++ common/src/main/java/us/myles/ViaVersion/exception/CancelException.java
@@ -1,8 +1,6 @@
 package us.myles.ViaVersion.exception;
 
-import io.netty.handler.codec.EncoderException;
-
-public class CancelException extends EncoderException { // Encoder exception as workaround for performance issue caused by (ab)using exceptions
+public class CancelException extends Exception {
     public static final CancelException CACHED = new CancelException("Cached - Enable /viaver debug to not use cached exception") {
         @Override
         public synchronized Throwable fillInStackTrace() {
@@ -23,5 +21,9 @@ public class CancelException extends EncoderException { // Encoder exception as 
 
     public CancelException(Throwable cause) {
         super(cause);
+    }
+
+    public CancelException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
     }
 }
